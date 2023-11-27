702b07cd8c5065677b429ceb90ce6a2619081cbf
Bobojon <bkaroev@transparent.com>
2023-11-27 17:55:35 +0500

    commit

--- android/app/src/main/kotlin/ru/ecoplantagro/com/MainActivity.kt
+++ android/app/src/main/kotlin/ru/ecoplantagro/com/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.website
+package ru.ecoplantagro.com
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
 import 'dart:async';
-import 'dart:io';
-
-import 'package:connectivity/connectivity.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:webview_flutter/webview_flutter.dart';
@@ -35,39 +31,31 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final Completer<WebViewController> _controller =
-      Completer<WebViewController>();
   final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
   int number = 0;
   bool hasInternet = true;
+  late final WebViewController _controller;
 
   @override
   void initState() {
-    InternetAddress.lookup('example.com').then((result) {
-      if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
-        print('connected');
-        setState(() {
-          hasInternet = true;
-        });
-      } else {
-        print('disconnected');
-        setState(() {
-          hasInternet = false;
-        });
-      }
-    }).catchError((err) {
-      setState(() {
-        hasInternet = false;
-      });
-    });
     super.initState();
-    if (Platform.isAndroid) WebView.platform = AndroidWebView();
-    _prefs.then((SharedPreferences prefs) {
-      if (prefs.getString('number')! != null) {
-        number = int.parse(prefs.getString('number')!);
-        setState(() {});
-      }
-    });
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            // Update loading bar.
+          },
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse('https://ru.ecoplantagro.com'));
   }
 
   @override
@@ -77,93 +65,10 @@ class _MyHomePageState extends State<MyHomePage> {
 
   initPref() async {}
 
-  JavascriptChannel _toasterJavascriptChannel(BuildContext context) {
-    return JavascriptChannel(
-        name: 'Toaster',
-        onMessageReceived: (JavascriptMessage message) {
-          Scaffold.of(context).showSnackBar(
-            SnackBar(content: Text(message.message)),
-          );
-        });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: hasInternet
-          ? SafeArea(
-              child: WebView(
-                initialUrl:
-                    'https://kyrgyz.space/p/ekobak/basket.php?user=$number',
-                javascriptMode: JavascriptMode.unrestricted,
-                onWebViewCreated: (WebViewController webViewController) {
-                  _prefs.then((SharedPreferences prefs) {
-                    if (prefs.getString('number') == null) {
-                      webViewController
-                          .loadUrl(
-                              'http://kyrgyz.space/p/ekobak/get_user_id.php')
-                          .then((value2) {
-                        if (kDebugMode) {
-                          print('==============');
-                        }
-                        return;
-                      });
-                    } else {
-                      number = int.parse(prefs.getString('number')!);
-                      webViewController
-                          .loadUrl(
-                              'https://kyrgyz.space/p/ekobak/basket.php?user=$number')
-                          .then((value2) {
-                        if (kDebugMode) {
-                          print('==============');
-                        }
-                        return;
-                      });
-                    }
-                  });
-                },
-                onProgress: (int progress) {
-                  print('WebView is loading (progress : $progress%)');
-                },
-                javascriptChannels: <JavascriptChannel>{
-                  _toasterJavascriptChannel(context),
-                },
-                navigationDelegate: (NavigationRequest request) {
-                  if (request.url.startsWith('https://www.youtube.com/')) {
-                    print('blocking navigation to $request}');
-                    return NavigationDecision.prevent;
-                  }
-                  print('allowing navigation to $request');
-                  return NavigationDecision.navigate;
-                },
-                onPageStarted: (String url) {
-                  print('Page started loading: $url');
-                },
-                onPageFinished: (String url) async {
-                  print('Page finished loading: $url');
-                  if (url != '' && url != null) {
-                    var index = url.indexOf('=');
-                    print('NUMBER ${index}');
-                    if (index != -1) {
-                      var number = url.substring(index + 1, url.length);
-                      print('NUMBER ${number}');
-                      _prefs.then((SharedPreferences prefs) {
-                        if (prefs.getString('number') == null &&
-                            number != '0') {
-                          this.number = int.parse(number);
-                          prefs.setString('number', number);
-                          setState(() {});
-                        }
-                      });
-                    }
-                  }
-                },
-                backgroundColor: const Color(0x00000000),
-              ),
-            )
-          : const Center(
-              child: Text('You internet no connection'),
-            ),
+      body: SafeArea(child: WebViewWidget(controller: _controller)),
     );
   }
 }
